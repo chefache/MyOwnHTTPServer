7a3ae394d4772f010312b46e0b394a9973e51e97
chefache <stefan.m.koev@gmail.com>
2020-10-03 08:36:04 +0300

    InitialCreate

---
+++ Apps/MyFirstMvcApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using SUS.HTTP;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstMvcApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var server = new HttpServer();
+
+            server.AddRoute("/", HomePage);
+            server.AddRoute("/about", About);
+            server.AddRoute("/user/login", LogIn);
+
+            server.Start(80);
+        }
+
+        static HttpResponse HomePage(HttpRequest request)
+        {
+            
+        }
+        static HttpResponse About(HttpRequest request)
+        {
+
+        }
+        static HttpResponse LogIn(HttpRequest request)
+        {
+
+        }
+    }
+}
