04a05fafbcef561db55c26667ae981a8af47e6ce
chefache <stefan.m.koev@gmail.com>
2020-10-03 23:40:54 +0300

    Implement Httprequest and cookies

---
+++ SUS.HTTP/Cookie.cs
@@ -0,0 +1,23 @@
+﻿namespace SUS.HTTP
+{
+    public class Cookie
+    {
+        private string cookiesAsString;
+
+        public Cookie(string cookiesAsString)
+        {
+            var cookieParts = cookiesAsString.Split(new char[] { '='}, 2);
+            this.Name = cookieParts[0];
+            this.Value = cookieParts[1];
+        }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Name}={this.Value}";
+        }
+    }
+}
--- SUS.HTTP/HttpServer.cs
+++ SUS.HTTP/HttpServer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -10,7 +11,7 @@ namespace SUS.HTTP
 {
     public class HttpServer : IHttpServer
     {
-        private const int BufferSize = 4096;
+       
 
         IDictionary<string, Func<HttpRequest, HttpResponse>>
             routeTable = new Dictionary<string, Func<HttpRequest, HttpResponse>>();
@@ -42,37 +43,64 @@ namespace SUS.HTTP
 
         private async Task ProcessClientAsync(TcpClient tcpClient)
         {
-            //TODO: Add using
-            using (NetworkStream stream = tcpClient.GetStream())
+            try
             {
-                List<byte> data = new List<byte>();
-                int position = 0;
-                byte[] buffer = new byte[BufferSize];
-
-                while (true)
+                //TODO: Add using
+                using (NetworkStream stream = tcpClient.GetStream())
                 {
-                    int count =
-                        await stream.ReadAsync(buffer, position, buffer.Length);
-                    position += count;
+                    List<byte> data = new List<byte>();
+                    int position = 0;
+                    byte[] buffer = new byte[HttpConstants.BufferSize];
 
-                    if (count < buffer.Length)
+                    while (true)
                     {
-                        var partialBuffer = new byte[count];
-                        Array.Copy(buffer, partialBuffer, count);
-                        data.AddRange(partialBuffer);
-                        break;
+                        int count =
+                            await stream.ReadAsync(buffer, position, buffer.Length);
+                        position += count;
+
+                        if (count < buffer.Length)
+                        {
+                            var partialBuffer = new byte[count];
+                            Array.Copy(buffer, partialBuffer, count);
+                            data.AddRange(partialBuffer);
+                            break;
+                        }
+                        else
+                        {
+                            data.AddRange(buffer);
+                        }
                     }
-                    else
-                    {
-                        data.AddRange(buffer);
-                    }
+                    var requestAsString = Encoding.UTF8.GetString(data.ToArray());
+
+                    var request = new HttpRequest(requestAsString);
+                    Console.WriteLine($"{request.Method} {request.Path} => {request.Headers.Count} headers.");
+
+                    //TODO: extract info requestAsString
+
+                    var responseHtml = "<h1>Welcome!</h1>" +
+                        request.Headers.FirstOrDefault(x => x.Name == "User-Agent")?.Value;
+                    var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
+
+                    var responseHttp = "HTTP/1.1 200 OK" + HttpConstants.NewLine +
+                        "Server: SUS Server 1.0" + HttpConstants.NewLine +
+                        "Content-Type: text/html" + HttpConstants.NewLine +
+                        "Content-Length: " + responseBodyBytes.Length + HttpConstants.NewLine +
+                       HttpConstants.NewLine;
+
+                    var responseHeaderBytes = Encoding.UTF8.GetBytes(responseHttp);
+
+                    await stream.WriteAsync(responseHeaderBytes, 0, responseHeaderBytes.Length);
+                    await stream.WriteAsync(responseBodyBytes, 0, responseBodyBytes.Length);
                 }
-                var requestAsString = Encoding.UTF8.GetString(data.ToArray());
 
-                Console.WriteLine(requestAsString);
+                tcpClient.Close();
+            }
+            catch (Exception ex)
+            {
 
-                // await stream.WriteAsync();
-            }      
+                Console.WriteLine(ex);
+            }
+           
         }
     }
 }
