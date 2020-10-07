765947819720b08f1bb9ad33b00114e8e1743345
chefache <stefan.m.koev@gmail.com>
2020-10-07 21:48:01 +0300

    Add CSS and Bootstrap

--- Apps/MyFirstMvcApp/Controllers/StaticFilesController.cs
+++ Apps/MyFirstMvcApp/Controllers/StaticFilesController.cs
@@ -7,11 +7,31 @@ using System.Text;
 
 namespace MyFirstMvcApp.Controllers
 {
-    public class StaticFilesController: Controller
+    public class StaticFilesController : Controller
     {
         public HttpResponse Favicon(HttpRequest arg)
         {
             return this.File("wwwroot/vb-net-icon-1.ico", "image/vnd.microsoft.icon");
+        }
+
+        internal HttpResponse BootstrapCss(HttpRequest arg)
+        {
+            return this.File("wwwroot/css/bootstrap.min.css", "text/css");
+        }
+
+        internal HttpResponse CustomCss(HttpRequest arg)
+        {
+            return this.File("wwwroot/css/custom.css", "text/css");
+        }
+
+        internal HttpResponse CustomJs(HttpRequest arg)
+        {
+            return this.File("wwwroot/js/custom.js", "text/javascript");
+        }
+
+        internal HttpResponse BootstrapJs(HttpRequest arg)
+        {
+            return this.File("wwwroot/js/bootstrap.bundle.min.js", "text/javascript");
         }
     }
 }
